f7fe550813655e60f95306be5330d961b8fc6683
Shubham Kumar <sraj94710@gmail.com>
2022-11-20 11:22:22 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,34 @@
+//set initial number
+let count = 0;
+
+//Select value and buttons
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach(function(button){
+    button.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        }
+        else if(styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+
+        if(count > 0){
+            value.style.color = "green";
+        }
+
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0){
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    });
+});
